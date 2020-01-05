0c34af1e85b2f3adfb36366bb1399824e1f9b140
fly <2439730644@qq.com>
2020-01-06 00:54:30 +0800

    集成redis、mybatis

---
+++ src/main/java/com/fly/util/MD5Util.java
@@ -0,0 +1,35 @@
+package com.fly.util;
+
+import org.apache.commons.codec.cli.Digest;
+import org.apache.commons.codec.digest.DigestUtils;
+
+public class MD5Util {
+
+    private static final String PUBLIC_SALT = "13589ACEGZ";
+
+
+    public static String md5(String plainStr) {
+        return DigestUtils.md5Hex(plainStr);
+    }
+
+    public static String encryptFromInput(String input) {
+        String s = PUBLIC_SALT.charAt(1) + PUBLIC_SALT.charAt(3) + input + PUBLIC_SALT.charAt(6) + PUBLIC_SALT.charAt(8);
+        return md5(s);
+    }
+
+    public static String encryptFormInputToDB(String form, String salt) {
+        String s = PUBLIC_SALT.charAt(1) + PUBLIC_SALT.charAt(3) + form + PUBLIC_SALT.charAt(6) + PUBLIC_SALT.charAt(8);
+        return md5(s);
+    }
+
+    public static String encryptPlainFormToDb(String plainInput, String salt) {
+        String s = encryptFromInput(plainInput);
+        return encryptFormInputToDB(s, salt);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(encryptFromInput("123456"));
+        System.out.println(encryptPlainFormToDb("123456", PUBLIC_SALT));
+    }
+
+}
